c68327bd0431c0a2a3f6e41abcb900f80ff0ea54
JGKing88 <jack.g.king@gmail.com>
2023-02-02 16:05:16 -0500

    dimesion updates

--- calculator.py
+++ calculator.py
@@ -1,12 +1,15 @@
+from itsdangerous import NoneAlgorithm
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+from text_generator import *
 
 
 # hyperparameters
 batch_size = 4 # how many independent sequences will we process in parallel?
-block_size = 16 # what is the maximum context length for predictions?
-max_iters = 1000
+block_size = 15 # what is the maximum context length for predictions?
+output_size = 5
+max_iters = 2000
 eval_interval = 500
 learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -14,40 +17,41 @@ eval_iters = 200
 n_embd = 32
 n_head = 4
 n_layer = 2
-dropout = 0.2
+dropout = 0.1
 # ------------
 
 torch.manual_seed(1337)
 
-with open('input.txt', 'r', encoding='utf-8') as f:
-    text = f.read()
+# with open('input.txt', 'r', encoding='utf-8') as f:
+#     text = f.read()
 
-print(len(text))
+# print(len(text))
 
-# here are all the unique characters that occur in this text
-chars = sorted(list(set(text)))
-vocab_size = len(chars)
-# create a mapping from characters to integers
-stoi = { ch:i for i,ch in enumerate(chars) }
-itos = { i:ch for i,ch in enumerate(chars) }
-encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
-decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+# # here are all the unique characters that occur in this text
+# chars = sorted(list(set(text)))
+# vocab_size = len(chars)
+# # create a mapping from characters to integers
+# stoi = { ch:i for i,ch in enumerate(chars) }
+# itos = { i:ch for i,ch in enumerate(chars) }
+# encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
+# decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
 
-# Train and test splits
-data = torch.tensor(encode(text), dtype=torch.long)
-n = int(0.9*len(data)) # first 90% will be train, rest val
-train_data = data[:n]
-val_data = data[n:]
+# # Train and test splits
+# data = torch.tensor(encode(text), dtype=torch.long)
+# n = int(0.9*len(data)) # first 90% will be train, rest val
+# train_data = data[:n]
+# val_data = data[n:]
 
-# data loading
-def get_batch(split):
-    # generate a small batch of data of inputs x and targets y
-    data = train_data if split == 'train' else val_data
-    ix = torch.randint(len(data) - block_size, (batch_size,))
-    x = torch.stack([data[i:i+block_size] for i in ix])
-    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
-    x, y = x.to(device), y.to(device)
-    return x, y
+# # data loading
+# def get_batch(split):
+#     # generate a small batch of data of inputs x and targets y
+#     data = train_data if split == 'train' else val_data
+#     ix = torch.randint(len(data) - block_size, (batch_size,))
+#     x = torch.stack([data[i:i+block_size] for i in ix])
+#     y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+#     x, y = x.to(device), y.to(device)
+#     return x, y
+    
 
 @torch.no_grad()
 def estimate_loss():
@@ -56,7 +60,7 @@ def estimate_loss():
     for split in ['train', 'val']:
         losses = torch.zeros(eval_iters)
         for k in range(eval_iters):
-            X, Y = get_batch(split)
+            X, Y = get_equation(block_size, output_size)
             logits, loss = model(X, Y)
             losses[k] = loss.item()
         out[split] = losses.mean()
@@ -145,6 +149,7 @@ class Transformer(nn.Module):
         self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
         self.ln_f = nn.LayerNorm(n_embd) # final layer norm
         self.lm_head = nn.Linear(n_embd, vocab_size)
+        self.lm_dim = nn.Linear(block_size, output_size)
 
     def forward(self, idx, targets=None):
         B, T = idx.shape
@@ -156,6 +161,9 @@ class Transformer(nn.Module):
         x = self.blocks(x) # (B,T,C)
         x = self.ln_f(x) # (B,T,C)
         logits = self.lm_head(x) # (B,T,vocab_size)
+        logits = logits.view(logits.size(0), logits.size(2), -1)
+        logits = self.lm_dim(logits)
+        logits = logits.view(logits.size(0), logits.size(2), -1)
 
         if targets is None:
             loss = None
@@ -163,7 +171,7 @@ class Transformer(nn.Module):
             B, T, C = logits.shape
             logits = logits.view(B*T, C)
             targets = targets.view(B*T)
-            loss = F.cross_entropy(logits, targets)
+            loss = F.cross_entropy(logits, targets, ignore_index=-1)
 
         return logits, loss
 
@@ -200,7 +208,8 @@ for iter in range(max_iters):
         print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
 
     # sample a batch of data
-    xb, yb = get_batch('train')
+    # xb, yb = get_batch('train')
+    xb,yb = get_equation(block_size, output_size)
 
     # evaluate the loss
     logits, loss = model(xb, yb)
@@ -208,15 +217,13 @@ for iter in range(max_iters):
     loss.backward()
     optimizer.step()
 
-# generate from the model
-# context = torch.zeros((1, 1), dtype=torch.long, device=device)
-# print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
-# open('more.txt', 'w').write(decode(m.generate(context, max_new_tokens=10000)[0].tolist()))
 
-text = encode("1 + 1")
-print(text)
+text = encode("        3 + 3 =")
 context = torch.tensor(text).view((1, -1))
-print(decode(m.generate(context, max_new_tokens=10)[0].tolist()))
-open('more.txt', 'w').write(decode(m.generate(context, max_new_tokens=10000)[0].tolist()))
+print(decode(m.generate(context, max_new_tokens=1)[0].tolist()))
+print(decode(m.generate(context, max_new_tokens=1)[0].tolist()))
+print(decode(m.generate(context, max_new_tokens=1)[0].tolist()))
+print(decode(m.generate(context, max_new_tokens=1)[0].tolist()))
+
 
 
--- text_generator.py
+++ text_generator.py
@@ -1,4 +1,5 @@
 import random
+import torch
 
 plus = lambda a, b: a + b
 minus = lambda a, b: a - b
@@ -7,16 +8,38 @@ div = lambda a, b: a / b
 ops = {0: plus, 1: minus, 2: mult, 3: div}
 opsign = {0: "+", 1: "-", 2: "*", 3: "/"}
 
-text = ""
+# here are all the unique characters that occur in this text
+chars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+", " ", "-", "/", "*", "\n", "="]
+vocab_size = len(chars)
+# create a mapping from characters to integers
+stoi = { ch:i for i,ch in enumerate(chars) }
+itos = { i:ch for i,ch in enumerate(chars) }
 
-for _ in range(100000):
-    a = random.randint(0, 10000)
-    b = random.randint(1, 10000)
+def encode(s):
+    return [stoi[c] for c in s] # encoder: take a string, output a list of integers
+
+def decode(l):
+    return ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+
+
+def get_equation(block_size, output_size):
+    a = random.randint(0, 100)
+    b = random.randint(1, 100)
     op = random.randint(0, 3)
     c = ops[op](a, b)
-    c = round(c, 9)
-    eq = str(a) + " " + opsign[op] + " " + str(b) + " = " + str(c) + "\n"
-    text += eq
+    c = round(c)
 
-with open('input.txt', 'w') as f:
-    f.write(text)
+    input = str(a) + " " + opsign[op] + " " + str(b) + " = "
+    dif = block_size - len(input)
+    input = torch.cat((torch.zeros(dif, dtype=torch.long), torch.tensor(encode(input), dtype = torch.long)))
+    
+    output = str(c)
+    dif = output_size - len(output)
+    output = torch.cat((torch.zeros(dif, dtype=torch.long) - 1, torch.tensor(encode(output), dtype = torch.long)))
+
+    output = output.view(1, -1)
+    input = input.view(1, -1)
+    return input, output
+
+if __name__ == "__main__":
+    print(get_equation())
